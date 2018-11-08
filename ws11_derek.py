523d47d6e23c9d602ad4b6fa2a9933f14e4767be
betaBison <knowles.derek@gmail.com>
2018-11-08 12:40:28 -0700

    plon change

--- ws11_derek.py
+++ ws11_derek.py
@@ -16,5 +16,5 @@ Blon = np.matrix([0],
                  [l1/(m1*l1**2+m2*l2**2+Jy)])
 Clon = np.matrix([1,0])
 
-plon = np.array([-zeta_theta*wn_theta+i*wn_theta*sqrt(1-zeta**2),\
--zeta_theta*wn_theta-i*wn_theta*sqrt(1-zeta**2)])
+plon = np.array([-zeta_theta*wn_theta+1j*wn_theta*sqrt(1-zeta**2),\
+-zeta_theta*wn_theta-1j*wn_theta*sqrt(1-zeta**2)])
